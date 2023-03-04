8848640e20d34aadf384cf3caad7ffe4e57bf2ca
Your Name <you@example.com>
2023-03-04 03:45:26 +0100

    Creation d'un site ecommerce

---
+++ monsite.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="monstyle.css"> 
+    <title>Site e-commerce</title>
+    
+</head>
+<body>
+    <!-- Barre de navigation -->
+    <nav>
+        <h1>Ib Shop</h1>
+        <div class="onglets">
+            <p class="link">Nouveautés</p>
+            <p class="link">Homme</p>
+            <p class="link">Femme</p>
+            <p class="link">Enfant</p>
+            <p class="link">Cadeaux</p>
+            <form>
+                <input type="search" name="search" id="" placeholder="Recherche">
+            </form>
+            <p><i class="fa-regular fa-heart"></i></p>
+            <p><i class="fa-solid fa-cart-shopping"></i></p>
+        </div>
+    </nav>
+    <!-- Fin de la barre de navigation -->
+    <!-- Notre header -->
+    <h1>C'est votre boutique, votre marque de futur.</h1>
+    <button>Naviguer <i class="fa-solid fa-paper-plane"></i></button>
+    <!-- Fin de header -->
+</body>
+</html>
